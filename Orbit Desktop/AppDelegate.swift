5655ac0c166ef487c1c8cda54a26edd95d94299e
Stefan Britton <stefan.britton@me.com>
2016-12-30 15:01:23 -0600

    Initial Commit

---
+++ Orbit Desktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Orbit Desktop
+//
+//  Created by Stefan Britton on 2016-12-30.
+//  Copyright © 2016 Kasama. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Orbit Desktop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Orbit Desktop
+//
+//  Created by Stefan Britton on 2016-12-30.
+//  Copyright © 2016 Kasama. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
