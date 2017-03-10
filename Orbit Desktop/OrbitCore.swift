582ce35f0e447d1f925146ca6b77a2e40c1301f4
Stefan Britton <stefan.britton@me.com>
2017-03-10 17:28:23 -0600

    first

---
+++ Orbit Desktop/OrbitCore.swift
@@ -0,0 +1,86 @@
+//
+//  OrbitCore.swift
+//  Orbit
+//
+//  Created by Stefan Britton on 2016-12-30.
+//  Copyright © 2016 Kasama. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class OrbitCore {
+    
+    static let main = OrbitCore()
+    
+    let publicDatabase = CKContainer.default().publicCloudDatabase
+    let queue = OperationQueue()
+    
+    var currentUserRecordID: CKRecordID?
+    
+    func cloudLogin(completion: @escaping (_ userRecordID: CKRecordID?) -> Void) {
+        CKContainer.default().accountStatus { (status, error) in
+            guard error == nil else { print(error.debugDescription); return }
+            if status == .noAccount {
+                completion(nil)
+            } else {
+                CKContainer.default().fetchUserRecordID(completionHandler: { (recordID, error) in
+                    self.currentUserRecordID = recordID
+                    DispatchQueue.main.sync(execute: {
+                        completion(recordID)
+                    })
+                })
+            }
+        }
+    }
+    
+    func createPlanet(name: String, completion: @escaping (_ planet: CKRecord?) -> Void) {
+        let newPlanet = CKRecord(recordType: "Planet")
+        newPlanet.setValue(name, forKey: "name")
+        let modifyOperation = CKModifyRecordsOperation(recordsToSave: [newPlanet], recordIDsToDelete: nil)
+        modifyOperation.database = CKContainer.default().publicCloudDatabase
+        modifyOperation.modifyRecordsCompletionBlock = { (savedRecords, deletedRecords, error) in
+            print(savedRecords!)
+            DispatchQueue.main.sync(execute: {
+                completion(savedRecords?.first)
+            })
+        }
+        self.queue.addOperation(modifyOperation)
+    }
+    
+    func fetchPlanet(name: String, completion: @escaping (_ planet: CKRecord?) -> Void) {
+        var planet: CKRecord?
+        let predicate = NSPredicate(format: "name = %@", name)
+        let query = CKQuery(recordType: "Planet", predicate: predicate)
+        let queryOperation = CKQueryOperation(query: query)
+        queryOperation.database = self.publicDatabase
+        queryOperation.resultsLimit = 1
+        queryOperation.recordFetchedBlock = { (record) in
+            planet = record
+        }
+        self.queue.addOperation(queryOperation)
+        queryOperation.queryCompletionBlock = { (cursor, error) in
+            DispatchQueue.main.sync(execute: { 
+                completion(planet)
+            })
+        }
+    }
+    
+    func fetchDefaultPlanet(completion: @escaping (_ planet: CKRecord?) -> Void) {
+        var planet: CKRecord?
+        let predicate = NSPredicate(format: "creatorUserRecordID = %@", currentUserRecordID!)
+        let query = CKQuery(recordType: "Planet", predicate: predicate)
+        let queryOperation = CKQueryOperation(query: query)
+        queryOperation.database = self.publicDatabase
+        queryOperation.resultsLimit = 1
+        queryOperation.recordFetchedBlock = { (record) in
+            planet = record
+        }
+        self.queue.addOperation(queryOperation)
+        queryOperation.queryCompletionBlock = { (cursor, error) in
+            DispatchQueue.main.sync(execute: {
+                completion(planet)
+            })
+        }
+    }
+}
--- Orbit Desktop/ViewController.swift
+++ Orbit Desktop/ViewController.swift
@@ -9,11 +9,20 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet weak var tableView: NSTableView!
+    
+    var beacon = OrbitBeacon(identifier: "DESKTOP APP")
+    var devices = [(id: String, range: NSNumber)]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        checkExistingPlanet()
+    }
+    
+    func checkExistingPlanet() {
+        self.beacon.delegate = self
+        self.beacon.startBroadcasting()
+        self.beacon.startDetecting()
     }
 
     override var representedObject: Any? {
@@ -21,7 +30,21 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
+extension ViewController: OrbitBeaconDelegate {
+    
+    func beacon(_ beacon: OrbitBeacon, bluetoothEnabled: Bool) {
+        print("bluetoothEnabled: \(bluetoothEnabled)")
+    }
+    
+    func beacon(_ beacon: OrbitBeacon, foundDevices: [String : Any]) {
+        print("foundDevices: \(foundDevices)")
+        devices = [(id: String, range: NSNumber)]()
+        for key in foundDevices.keys {
+            let range = foundDevices[key] as! NSNumber
+            devices.append((id: key, range: range))
+        }
+//        tableView.reloadData()
+    }
+}
